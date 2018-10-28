a917f0911fab6d51ef8d85e00e41501cd8e4694b
Silicasandwhich <43558271+Silicasandwhich@users.noreply.github.com>
2018-10-28 13:57:14 -0400

    created game over screen

--- space shooter.py
+++ space shooter.py
@@ -13,6 +13,7 @@ lives = 3
 sessionHighSpeed = 1
 highScore = 0
 highSpeed = 1
+screen = "Game"
 
 shots = []
 enemies = [[128, 50]]
@@ -28,6 +29,8 @@ def spawn_enemy():
 
 def update():
     # globals
+    global highScore
+    global highSpeed
     global speedScore
     global playerX
     global shots
@@ -37,79 +40,97 @@ def update():
     global score
     global lives
     global sessionHighSpeed
-    # player input
-    if pyxel.btn(pyxel.KEY_D):
-        playerX += 3
-    if pyxel.btn(pyxel.KEY_A):
-        playerX -= 3
-    if pyxel.btnp(pyxel.KEY_SPACE):
-        shoot()
-    # screen boundaries
-    if playerX >= 255:
-        playerX = 255
-    elif playerX <= 0:
-        playerX = 0
-    # moving shots
-    for x in shots:
-        x[1] -= 5
-    # spawn enemies
-    spawnCounter += speedScore / 500 + 1
-    if spawnCounter >= 100:
-        spawnCounter = 0
-        spawn_enemy()
-    # move enemies
-    moveCounter += speedScore / 500 + 1
-    if moveCounter >= 50:
-        moveCounter = 0
+    global screen
+    if screen == "Game":
+        # player input
+        if pyxel.btn(pyxel.KEY_D):
+            playerX += 3
+        if pyxel.btn(pyxel.KEY_A):
+            playerX -= 3
+        if pyxel.btnp(pyxel.KEY_SPACE):
+            shoot()
+        # screen boundaries
+        if playerX >= 255:
+            playerX = 255
+        elif playerX <= 0:
+            playerX = 0
+        # moving shots
+        for x in shots:
+            x[1] -= 5
+        # spawn enemies
+        spawnCounter += speedScore / 500 + 1
+        if spawnCounter >= 100:
+            spawnCounter = 0
+            spawn_enemy()
+        # move enemies
+        moveCounter += speedScore / 500 + 1
+        if moveCounter >= 50:
+            moveCounter = 0
+            for enemy in enemies:
+                enemy[0] += random.randint(-30, 30)
+                enemy[1] += 10
+                if enemy[0] >= 255:
+                    enemy[0] = 255
+                if enemy[0] <= 0:
+                    enemy[0] = 0
+        # collision detection
         for enemy in enemies:
-            enemy[0] += random.randint(-30, 30)
-            enemy[1] += 10
-            if enemy[0] >= 255:
-                enemy[0] = 255
-            if enemy[0] <= 0:
-                enemy[0] = 0
-    # collision detection
-    for enemy in enemies:
-        for shot in shots:
-            if enemy[0] + 4 >= shot[0] >= enemy[0] - 4:
-                if enemy[1] + 4 >= shot[1] >= enemy[1] - 4:
-                    shots.remove(shot)
-                    enemies.remove(enemy)
-                    score += 50
-                    speedScore += 50
-    # lose a life if an enemy passes the player
-    for enemy in enemies:
-        if enemy[1] >= 200:
-            enemies.remove(enemy)
-            lives -= 1
-            if lives < 0:
-                lives = 0
-            speedScore -= 500
-            if speedScore < 0:
-                speedScore = 0
-    # change session high speed
-    if sessionHighSpeed < speedScore / 500 + 1:
-        sessionHighSpeed = speedScore
+            for shot in shots:
+                if enemy[0] + 4 >= shot[0] >= enemy[0] - 4:
+                    if enemy[1] + 4 >= shot[1] >= enemy[1] - 4:
+                        shots.remove(shot)
+                        enemies.remove(enemy)
+                        score += 50
+                        speedScore += 50
+        # lose a life if an enemy passes the player
+        for enemy in enemies:
+            if enemy[1] >= 200:
+                enemies.remove(enemy)
+                lives -= 1
+                if lives < 0:
+                    lives = 0
+                    if score > highScore:
+                        highScore = score
+                    if sessionHighSpeed > highSpeed:
+                        highSpeed = sessionHighSpeed
+                    screen = "Game Over"
+                speedScore -= 500
+                if speedScore < 0:
+                    speedScore = 0
+        # change session high speed
+        if sessionHighSpeed < speedScore / 500 + 1:
+            sessionHighSpeed = speedScore
+    elif screen == "Game Over":
+        if pyxel.btnp(pyxel.KEY_Z):
+            screen = "Start"
 
 
 def draw():
-    pyxel.cls(0)
-    # player
-    pyxel.circ(playerX, playerY, 5, 3)
-    # shots
-    for x in shots:
-        pyxel.line(x[0], x[1], x[0], x[1] + 7, 2)
-    # enemies
-    for enemy in enemies:
-        pyxel.circ(enemy[0], enemy[1], 3, 7)
-    # score
-    pyxel.text(15, 230, "Score: " + str(score), 7)
+    if screen == "Game":
+        pyxel.cls(0)
+        # player
+        pyxel.circ(playerX, playerY, 5, 3)
+        # shots
+        for x in shots:
+            pyxel.line(x[0], x[1], x[0], x[1] + 7, 2)
+        # enemies
+        for enemy in enemies:
+            pyxel.circ(enemy[0], enemy[1], 3, 7)
+        # score
+        pyxel.text(15, 230, "Score: " + str(score), 7)
 
-    # lives
-    pyxel.text(210, 230, "Lives: " + str(lives), 7)
+        # lives
+        pyxel.text(210, 230, "Lives: " + str(lives), 7)
 
-    # speed
-    pyxel.text(210, 25, "Speed: " + str(round(speedScore / 500 + 1, 2)) + 'x', 7)
+        # speed
+        pyxel.text(210, 25, "Speed: " + str(round(speedScore / 500 + 1, 2)) + 'x', 7)
+    if screen == "Game Over":
+        pyxel.cls(0)
+        pyxel.text(120, 55, "Game Over", 7)
+        pyxel.text(75, 78, "Score: " + str(score), 7)
+        pyxel.text(175, 78, "High: " + str(highScore), 7)
+        pyxel.text(75, 140, "Speed: " + str(sessionHighSpeed) + 'x', 7)
+        pyxel.text(175, 140, "High: " + str(highSpeed) + 'x', 7)
 
 
 pyxel.run(update, draw)
